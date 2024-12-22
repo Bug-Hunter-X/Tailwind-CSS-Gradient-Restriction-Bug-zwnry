1bd4c865181f322bed1a5703240c2fcc1f513037
BugHunter-X <askzenai@gmail.com>
2024-12-22 20:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example showing the issue with Tailwind's arbitrary value restriction.
+<div class="bg-gradient-to-r from-red-500 to-blue-500 h-screen w-screen">
+  {/* Content here */}
+</div>
+```
